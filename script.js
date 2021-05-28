bf68aa62af3be36eddc12f38f13717a5e6183964
Joseph Manning <jgmanning3@yahoo.com>
2021-05-27 20:36:28 -0700

    Created assignment codes

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = lowerCase.toUpperCase();
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// var specialChar = ["!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?"]
+
+var charAmount = 
+var includeLower = 
+var includeUpper =
+var includeNumeric =
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +21,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
