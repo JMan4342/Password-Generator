82a8c01b4994262fce7a215441858fdf483e9acc
Joseph Manning <jgmanning3@yahoo.com>
2021-05-29 09:48:15 -0700

    Try to get results from prompts to populate in Js

--- script.js
+++ script.js
@@ -5,58 +5,72 @@ var upperCase = lowerCase.toUpperCase();
 var numeric = "0123456789";
 var symbol = '+-:;?_~!@#$%^&*`|"(){}[]=<> /,.';
 
-//Criteria from the prompt boxes
-var charAmount = 20;
-var includeLower = true;
-var includeUpper = true;
-var includeNumeric = true;
-var includeSymbol = true;              
+var characterInput = document.querySelector("#generate")
+var lowerInput = document.querySelector("#generate")
+var upperInput = document.querySelector("#generate")
+var numberInput = document.querySelector("#generate")
+var symbolInput =   document.querySelector("#generate")
+
+characterInput.addEventListener("click", 
+function(){prompt("How long would do you want your passcode", "12")});
+// while (characterInput < 8 || characterInput > 128 || isNaN(characterInput) || characterInput == null) {
+//   alert("Enter a value between 8 and 128")
+//   prompt("How long would do you want your passcode", "12")}})
+
+lowerInput.addEventListener("click", function(){confirm('Do you want lower case letters in your password?\nOK for "Yes"\nCancel for "No"')})
+upperInput.addEventListener("click", function(){confirm('Do you want upper case letters in your password?\nOK for "Yes"\nCancel for "No"')})
+numberInput.addEventListener("click", function(){confirm('Do you want numbers in your password?\nOK for "Yes"\nCancel for "No"')})
+symbolInput.addEventListener("click", function(){confirm('Do you want special characters in your password?\nOK for "Yes"\nCancel for "No"')})
+
+var charAmount = characterInput;
+var includeLower = lowerInput;
+var includeUpper = upperInput;
+var includeNumeric = numberInput;
+var includeSymbol = symbolInput;
 
 //Adding character criteria to a string
 var passCriteria = lowerCase;
-  // if (includeLower = true) {return lowerCase},
-  if (includeUpper) passCriteria = passCriteria + upperCase;
-  if (includeNumeric) passCriteria = passCriteria + numeric;
-  if (includeSymbol) passCriteria = passCriteria + symbol; 
+// if (includeLower = true) {return lowerCase},
+if (includeUpper) passCriteria = passCriteria + upperCase;
+if (includeNumeric) passCriteria = passCriteria + numeric;
+if (includeSymbol) passCriteria = passCriteria + symbol; 
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
 
 function generatePassword() {
   var result = ""
-for (let i = 0; i < charAmount; i++) {
-  result += passCriteria[Math.floor(Math.random() * passCriteria.length)];
+  for (let i = 0; i < charAmount; i++) {
+    result += passCriteria[Math.floor(Math.random() * passCriteria.length)];
+  }
+  // return result
+  document.getElementById('password').innerHTML = result
 }
-return result
-}
-
+// console.log(generatePassword())
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
 
-function writePassword() {
-  var characterInput = prompt("How long would do you want your passcode", "12");
-  while(characterInput < 8) {
-    alert("Enter a value between 8 and 128")
-    var characterInput = prompt("How long would do you want your passcode", "12");
-  }
-  while(characterInput > 128) {
-    alert("Enter a value between 8 and 128")
-    var characterInput = prompt("How long would do you want your passcode", "12");
-  }
-  confirm('Do you want lower case letters in your password?\nOK for "Yes"\nCancel for "No"');
-  confirm('Do you want upper case letters in your password?\nOK for "Yes"\nCancel for "No"');
-  confirm('Do you want numbers in your password?\nOK for "Yes"\nCancel for "No"');
-  confirm('Do you want special characters in your password?\nOK for "Yes"\nCancel for "No"');
-  alert("Thank you");
-}
 
+// function writePassword() {
+//   var characterInput = prompt("How long would do you want your passcode", "12");
+//   while (characterInput < 8 || characterInput > 128 || isNaN(characterInput) || characterInput == null) {
+//     alert("Enter a value between 8 and 128")
+//     var characterInput = prompt("How long would do you want your passcode", "12")};
+  // var lowerInput = confirm('Do you want lower case letters in your password?\nOK for "Yes"\nCancel for "No"');
+//   var upperInput = confirm('Do you want upper case letters in your password?\nOK for "Yes"\nCancel for "No"');
+//   var numberInput = confirm('Do you want numbers in your password?\nOK for "Yes"\nCancel for "No"');
+//   var symbolInput = confirm('Do you want special characters in your password?\nOK for "Yes"\nCancel for "No"');
+//   alert("Thank you");
+//     return (characterInput, lowerInput, upperInput, numberInput, symbolInput)
+//   }
+// console.log(characterInput)
 
-// Confirm box to enter password criteria
+  //Criteria from the prompt boxes
